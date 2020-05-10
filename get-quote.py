5c4a77509dcbb0b17b4a49bc1218a789b6ea431d
ScottieX <christopherdrowley@gmail.com>
2020-05-10 23:41:06 +0100

    completed next steps

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,31 @@
 import random
 
-def primary():
+def add_quote(write_to_file):
+  user_prompt = input("Add new quote? (Y/N) ")
+
+  if user_prompt in ('Y','Yes','yes','YES'):  
+    with open(write_to_file,mode = 'a') as f:
+      new_quote = input("Add a new quote: ")
+      f.write(new_quote)
+  else:
+    return
+
+def primary(uf):
   print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
-  last = len(quotes)-1
-  rnd = random.randint(0,last)
-
-  print(quotes[rnd])
+  with open(uf,mode = 'r') as f:
+    quotes = f.readlines()
+    last = len(quotes)-1
+    rnd = random.randint(0,last)
+    return print(quotes[rnd])
+ 
 
 if __name__== "__main__":
-  primary()
+  uf = str(input('File name: '))
+  add_quote(uf)
   
+  uq_no = int(input("Number of quotes to display: "))
+
+  while uq_no > 0:
+    primary(uf)
+    uq_no -= 1
